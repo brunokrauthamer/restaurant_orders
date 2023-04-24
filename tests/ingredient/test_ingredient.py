3103fa306fd02727705f6f9a973f6e79cd436f34
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-04-24 16:55:52 -0300

    req01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1 starting
 def test_ingredient():
-    pass
+    presunto_ingredient1 = Ingredient('presunto')
+    presunto_ingredient2 = Ingredient('presunto')
+    assert int(presunto_ingredient1.__hash__()) == int(presunto_ingredient2.__hash__())
+    salmao_ingredient = Ingredient('salmão')
+    assert presunto_ingredient1.__hash__() != salmao_ingredient.__hash__()
+    assert presunto_ingredient1 == presunto_ingredient2
+    assert presunto_ingredient1 != salmao_ingredient
+    assert presunto_ingredient1.__repr__() == "Ingredient('presunto')"
+    assert presunto_ingredient1.name is "presunto"
+    assert (Restriction.ANIMAL_DERIVED in presunto_ingredient1.restrictions) == True
+    assert (Restriction.ANIMAL_MEAT in presunto_ingredient1.restrictions) == True
